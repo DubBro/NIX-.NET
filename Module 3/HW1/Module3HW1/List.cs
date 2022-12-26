93415800dde44e15bc2701dcb546e2ec55ae76d3
DubBro <taras.melnyk.aka.krimyllex@gmail.com>
2022-12-26 07:34:34 +0200

    Module 3. Homework 1 implemented

---
+++ Module 3/HW1/Module3HW1/List.cs
@@ -0,0 +1,165 @@
+﻿// <copyright file="List.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Module3HW1
+{
+    /// <summary>
+    /// List class.
+    /// </summary>
+    /// <typeparam name="T">The type of the items.</typeparam>
+    public class List<T> : IList<T>
+    {
+        private T[] list = Array.Empty<T>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="List{T}"/> class.
+        /// </summary>
+        public List()
+        {
+        }
+
+        /// <summary>
+        /// Gets count of items.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return this.list.Length;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets item value by its index.
+        /// </summary>
+        /// <param name="index">Index of the item.</param>
+        /// <returns>Item.</returns>
+        /// <exception cref="IndexOutOfRangeException">Index is out of range.</exception>
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return this.list[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                this.list[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// Adds an item to the list.
+        /// </summary>
+        /// <param name="item">Item that will be added to the list.</param>
+        public void Add(T item)
+        {
+            var tmpList = new T[this.Count + 1];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                tmpList[i] = this.list[i];
+            }
+
+            tmpList[this.Count] = item;
+
+            this.list = tmpList;
+        }
+
+        /// <summary>
+        /// Adds an array of items to the list.
+        /// </summary>
+        /// <param name="array">Items that will be added to the list.</param>
+        public void AddRange(T[] array)
+        {
+            var tmpList = new T[this.Count + array.Length];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                tmpList[i] = this.list[i];
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                tmpList[this.Count + i] = array[i];
+            }
+
+            this.list = tmpList;
+        }
+
+        /// <summary>
+        /// Adds a collection of items to the list.
+        /// </summary>
+        /// <param name="collection">Items that will be added to the list.</param>
+        public void AddRange(ICollection<T> collection)
+        {
+            var tmpArray = new T[collection.Count];
+
+            collection.CopyTo(tmpArray, 0);
+
+            this.AddRange(tmpArray);
+        }
+
+        /// <summary>
+        /// Removes first occurrence of the item.
+        /// </summary>
+        /// <param name="item">Item that will be removed.</param>
+        /// <returns>true if item was removed or false if not.</returns>
+        public bool Remove(T item)
+        {
+            int index = Array.IndexOf(this.list, item);
+
+            return this.RemoveAt(index);
+        }
+
+        /// <summary>
+        /// Removes first occurrence of the item by its index.
+        /// </summary>
+        /// <param name="index">Index of the item that will be removed.</param>
+        /// <returns>true if item was removed or false if not.</returns>
+        public bool RemoveAt(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                return false;
+            }
+
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.list[i] = this[i + 1];
+            }
+
+            Array.Resize(ref this.list, this.Count - 1);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Sorts the list.
+        /// </summary>
+        public void Sort()
+        {
+            Array.Sort(this.list);
+        }
+
+        /// <summary>
+        /// Gets an enumerator that iterates through the list.
+        /// </summary>
+        /// <returns>Enumerator.</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ListEnumerator<T>(this.list);
+        }
+    }
+}
---
+++ Module 3/HW1/Module3HW1/ListEnumerator.cs
@@ -0,0 +1,83 @@
+﻿// <copyright file="ListEnumerator.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Module3HW1
+{
+    using System.Collections;
+
+    /// <summary>
+    /// ListEnumerator class.
+    /// </summary>
+    /// <typeparam name="T">The type of the items.</typeparam>
+    public class ListEnumerator<T> : IEnumerator<T>
+    {
+        private T[] list;
+        private int position = -1;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListEnumerator{T}"/> class.
+        /// </summary>
+        /// <param name="list">The array of the items.</param>
+        public ListEnumerator(T[] list)
+        {
+            this.list = list;
+        }
+
+        /// <summary>
+        /// Gets the element in the collection at the current position of the enumerator.
+        /// </summary>
+        /// <returns>The element in the collection at the current position of the enumerator.</returns>
+        public T Current
+        {
+            get
+            {
+                if (this.position == -1 || this.position >= this.list.Length)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.list[this.position];
+            }
+        }
+
+        /// <summary>
+        /// Gets the element in the collection at the current position of the enumerator.
+        /// </summary>
+        /// <returns>The element in the collection at the current position of the enumerator.</returns>
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        /// <summary>
+        /// Advances the enumerator to the next element of the collection.
+        /// </summary>
+        /// <returns>
+        /// true if the enumerator was successfully advanced to the next element;
+        /// false if the enumerator has passed the end of the collection.
+        /// </returns>
+        public bool MoveNext()
+        {
+            if (this.position < this.list.Length - 1)
+            {
+                this.position++;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///  Sets the enumerator to its initial position, which is before the first element in the collection.
+        /// </summary>
+        public void Reset()
+        {
+            this.position = -1;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+    }
+}
