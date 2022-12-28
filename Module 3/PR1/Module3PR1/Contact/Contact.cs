659fe7d970dbc9be9866b1a9f1dbfea9b66f562e
DubBro <taras.melnyk.aka.krimyllex@gmail.com>
2022-12-28 14:28:13 +0200

    Module 3. Practice 1 implemented

---
+++ Module 3/PR1/Module3PR1/Contact/Contact.cs
@@ -0,0 +1,83 @@
+﻿// <copyright file="Contact.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Module3PR1.Contact
+{
+    using System.Globalization;
+    using Module3PR1.Contact.Interfaces;
+
+    /// <summary>
+    /// Contact class.
+    /// </summary>
+    public class Contact : IContact
+    {
+        private string name;
+        private string phoneNumber;
+        private CultureInfo culture = new CultureInfo("en");
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Contact"/> class.
+        /// </summary>
+        /// <param name="name">Contact's name.</param>
+        /// <param name="phoneNumber">>Contact's number.</param>
+        public Contact(string name, string phoneNumber)
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Contact"/> class.
+        /// </summary>
+        /// <param name="name">Contact's name.</param>
+        /// <param name="phoneNumber">>Contact's number.</param>
+        /// <param name="culture">>Contact's culture.</param>
+        public Contact(string name, string phoneNumber, CultureInfo culture)
+            : this(name, phoneNumber)
+        {
+            this.culture = culture;
+        }
+
+        /// <summary>
+        /// Gets the name of the contact.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        /// <summary>
+        /// Gets the phone number of the contact.
+        /// </summary>
+        public string PhoneNumber
+        {
+            get
+            {
+                return this.phoneNumber;
+            }
+        }
+
+        /// <summary>
+        /// Compares the current instance with another object of the same type and returns
+        /// an integer that indicates whether the current instance precedes, follows, or
+        /// occurs in the same position in the sort order as the other object.
+        /// </summary>
+        /// <param name="incomingcontact">An object to compare with this instance.</param>
+        /// <returns>
+        /// A value that indicates the relative order of the objects being compared.
+        /// The return value has these meanings:
+        /// Value – Meaning
+        /// Less than zero – This instance precedes other in the sort order.
+        /// Zero – This instance occurs in the same position in the sort order as other.
+        /// Greater than zero – This instance follows other in the sort order.
+        /// </returns>
+        public int CompareTo(IContact? incomingcontact)
+        {
+            return string.Compare(this.Name, incomingcontact?.Name, this.culture, CompareOptions.None);
+        }
+    }
+}
---
+++ Module 3/PR1/Module3PR1/ContactsCollection/ContactsCollectionEnumerator.cs
@@ -0,0 +1,82 @@
+﻿// <copyright file="ContactsCollectionEnumerator.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Module3PR1.ContactsCollection
+{
+    using System.Collections;
+    using Module3PR1.Contact.Interfaces;
+
+    /// <summary>
+    /// ContactsCollectionEnumerator class.
+    /// </summary>
+    public class ContactsCollectionEnumerator : IEnumerator<IContact>
+    {
+        private IContact[] contacts;
+        private int position = -1;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContactsCollectionEnumerator"/> class.
+        /// </summary>
+        /// <param name="contacts">Array of contacts.</param>
+        public ContactsCollectionEnumerator(IContact[] contacts)
+        {
+            this.contacts = contacts;
+        }
+
+        /// <summary>
+        ///  Gets the element in the collection at the current position of the enumerator.
+        /// </summary>
+        /// <returns>The element in the collection at the current position of the enumerator.</returns>
+        public IContact Current
+        {
+            get
+            {
+                if (this.position < 0 || this.position >= this.contacts.Length)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.contacts[this.position];
+            }
+        }
+
+        /// <summary>
+        ///  Gets the element in the collection at the current position of the enumerator.
+        /// </summary>
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        /// <summary>
+        /// Advances the enumerator to the next element of the collection.
+        /// </summary>
+        /// <returns>
+        /// true if the enumerator was successfully advanced to the next element;
+        /// false if the enumerator has passed the end of the collection.
+        /// </returns>
+        public bool MoveNext()
+        {
+            if (this.position < this.contacts.Length - 1)
+            {
+                this.position++;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Sets the enumerator to its initial position, which is before the first element in the collection.
+        /// </summary>
+        public void Reset()
+        {
+            this.position = -1;
+        }
+    }
+}
