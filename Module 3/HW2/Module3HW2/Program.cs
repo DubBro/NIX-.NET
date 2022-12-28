7871617eb4759c357fe44611fe216b21496578af
DubBro <taras.melnyk.aka.krimyllex@gmail.com>
2022-12-29 01:03:16 +0200

    Module 3. Homework 2 implemented

---
+++ Module 3/HW2/Module3HW2/Program.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Module3HW2
+{
+    /// <summary>
+    /// Program class.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main fucntion.
+        /// </summary>
+        /// <param name="args">The array of string arguments.</param>
+        public static void Main(string[] args)
+        {
+            FirstClass firstClass = new FirstClass();
+            SecondClass secondClass = new SecondClass();
+
+            firstClass.ShowDelegate(secondClass.Calc(firstClass.Pow, 5, 6)(7));
+        }
+
+        /// <summary>
+        /// Shows the result.
+        /// </summary>
+        /// <param name="a">Result.</param>
+        public static void Show(bool a)
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Module 3/HW2/Module3HW2/SecondClass.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="SecondClass.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Module3HW2
+{
+    /// <summary>
+    /// SecondClass class.
+    /// </summary>
+    public class SecondClass
+    {
+        private int powResult;
+
+        /// <summary>
+        /// Calls the Pow() method of the FirstClass using delegate
+        /// and returns the delegate of the method Result().
+        /// </summary>
+        /// <param name="powDelegate">The delegate of the method FirstClass.Pow().</param>
+        /// <param name="a">First number.</param>
+        /// <param name="b">Secod number.</param>
+        /// <returns>The delegate of the method Result().</returns>
+        public Func<int, bool> Calc(Func<int, int, int> powDelegate, int a, int b)
+        {
+            this.powResult = powDelegate(a, b);
+
+            Func<int, bool> resultDelegate = this.Result;
+
+            return resultDelegate;
+        }
+
+        private bool Result(int a)
+        {
+            return this.powResult % a == 0;
+        }
+    }
+}
