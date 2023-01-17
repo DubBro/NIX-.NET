902974ca2f7f7bd77fa230c8ca5de75903dce047
DubBro <taras.melnyk.aka.krimyllex@gmail.com>
2023-01-17 13:34:44 +0200

    Module 4. Homework 3 implemented

---
+++ Module 4/HW3/Module4HW3/Entities/Configs/EmployeeProjectConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW3.Entities.Instances;
+
+namespace Module4HW3.Entities.Configs
+{
+    public class EmployeeProjectConfiguration : IEntityTypeConfiguration<EmployeeProject>
+    {
+        public void Configure(EntityTypeBuilder<EmployeeProject> builder)
+        {
+            builder.ToTable("EmployeeProject");
+            builder.HasKey(ep => ep.EmployeeProjectId);
+            builder.Property(ep => ep.Rate).HasColumnType("money").IsRequired();
+            builder.Property(ep => ep.StartedDate).HasColumnType("datetime2").HasMaxLength(7).IsRequired();
+            builder.Property(ep => ep.EmployeeId).IsRequired();
+            builder.Property(ep => ep.ProjectId).IsRequired();
+
+            builder.HasOne(ep => ep.Employee)
+                   .WithMany(e => e.EmployeeProjects)
+                   .HasForeignKey(ep => ep.EmployeeId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(ep => ep.Project)
+                   .WithMany(p => p.EmployeeProjects)
+                   .HasForeignKey(ep => ep.ProjectId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Module 4/HW3/Module4HW3/Entities/Configs/ProjectConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW3.Entities.Instances;
+
+namespace Module4HW3.Entities.Configs
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.ToTable("Project");
+            builder.HasKey(p => p.ProjectId);
+            builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Budget).HasColumnType("money").IsRequired();
+            builder.Property(p => p.StartedDate).HasColumnType("datetime2").HasMaxLength(7).IsRequired();
+
+            builder.HasMany(p => p.EmployeeProjects)
+                   .WithOne(ep => ep.Project)
+                   .HasForeignKey(ep => ep.ProjectId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
